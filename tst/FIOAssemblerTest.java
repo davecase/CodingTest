cc54798c91fc4aaa15f3fe80de0f749233325a10
Dave Case <dave.case@gmail.com>
2013-08-27 23:37:58 -0600

    iniial setup of unit test tree

---
+++ tst/FIOAssemblerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FIOAssemblerTest {
+
+	@Test
+	public void testFIOAssembler() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
